c872dba5779fb6f9d4c6c04cc2012713ea541b61
Kuldip Singh Rathore <kuldeepsinghmertiya1996@gmail.com>
2025-07-30 03:34:38 +0530

    aaa

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // backend/routes/authRoutes.js
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authcontroller");
+const { register, login } = require("../controllers/authController");
 
 router.post("/login", login);
 
